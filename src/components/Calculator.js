a0ba466e5f8f8ecede58617467d24156c00df710
AvielDez <110438720+AvielDez@users.noreply.github.com>
2023-02-20 20:06:47 -0600

    Task: Save Input To State
    The user can click the calculator buttons or use the keyboard to input numbers

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,6 +3,25 @@ import Header from './Header'
 import Interface from './Interface'
 
 export default function Calculator({toggle, togglePosition}){
+    const [userInput, setUserInput] = React.useState('')
+   
+    React.useEffect(()=>{
+        document.addEventListener('keypress', handleInput)
+        return () => {
+            document.removeEventListener('keypress', handleInput)
+        }
+        function handleInput(event){
+            if(!isNaN(event.key)){
+                setUserInput(prevUserInput => prevUserInput + event.key)
+            }
+        }
+    }, [userInput])
+
+
+    function keyInput(input){
+        setUserInput(prevUserInput => prevUserInput + input)
+    }
+
     return(
         <div className='calculator'>
             <Header 
@@ -10,6 +29,8 @@ export default function Calculator({toggle, togglePosition}){
                 togglePosition={togglePosition}
             />
             <Interface
+                keyInput = {keyInput}
+                userInput = {userInput}
                 togglePosition={togglePosition}
             />
         </div>
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Display({togglePosition}){
+export default function Display({togglePosition, userInput}){
     return(
         <div className={`display display-${togglePosition}`}>
             <p className='display__upper-line'>20 x 20 =</p>
-            <p className='display__main-line'>400</p>
+            <p className='display__main-line'>{userInput}</p>
         </div>
     )
 }
